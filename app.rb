6d6911f384dd62cf7cac57bda75b77cb377193fa
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-06-05 16:15:42 -0400

    Pass tests

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
 require './environment'
+require "pry"
 
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get "/" do
+      erb :root
+    end
 
+    get "/new" do
+      erb :"pirates/new"
+    end
+
+    post "/pirates" do
+      @pirate = Pirate.new(params[:pirate])
+      @ships = params[:pirate][:ships].map do |ship|
+        Ship.new(ship)
+      end
+
+      erb :"pirates/show"
+    end
   end
 end
